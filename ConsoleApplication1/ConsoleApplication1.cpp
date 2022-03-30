3c3ab4d161b023e8d42ff8c9da32017570783b8b
ertanay <ertanay8@gmail.com>
2022-03-30 15:33:13 +0300

    Ortalama Hesaplama
    C++ ile sınav puanları girilen sınıfın ortalamasını hesaplama.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Turkish");
+	int toplam,notsayaci,notu,ortalama;
+	toplam = 0;
+	notsayaci = 0;
+		while (notsayaci<2)
+		{
+			cout << "Notu Giriniz: ";
+			cin >> notu;
+			toplam = toplam + notu;
+			notsayaci = notsayaci + 1;
+
+		}
+	ortalama = toplam / notsayaci;
+	cout << "Sınıf Ortalaması " << ortalama << endl;
+	return 0;
+}
